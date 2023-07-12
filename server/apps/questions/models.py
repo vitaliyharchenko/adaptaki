13af00b2cf2bedc0bedf823ad7a9936cfb9a7c98
Vitaliy <harchenko.grape@gmail.com>
2023-07-12 13:47:16 +0500

    replace apps & add questions functionality

---
+++ server/apps/questions/models.py
@@ -0,0 +1,130 @@
+from django.db import models
+from markdownx.models import MarkdownxField
+
+
+def get_question_image_directory_path(instance, filename):
+    return f'questions/question_{instance.pk}/{filename}'
+
+
+# политики проверки
+ONE_WRONG_MINUS_ONE = 1
+ONE_WRONG_MINUS_ALL = 2
+
+POLICY_CHOICES = [
+    (ONE_WRONG_MINUS_ONE, 'Один неверный ответ, минус один балл.'),
+    (ONE_WRONG_MINUS_ALL, 'Один неверный ответ, минус все баллы.'),
+]
+
+# типы заданий
+SIMPLE = 1
+ORDERED_SYMBOLS = 2
+UNORDERED_SYMBOLS = 3
+ONE_CHOICE = 4
+MANY_CHOICE = 5
+COMPOSITE = 6
+
+TYPE_CHOICES = [
+    (SIMPLE, 'Обычная проверка строки'),
+    (ORDERED_SYMBOLS, 'Набор символов в строгом порядке'),
+    (UNORDERED_SYMBOLS, 'Набор символов в случайном порядке'),
+    (ONE_CHOICE, 'Выбор одного верного ответа (с кнопками)'),
+    (MANY_CHOICE, 'Выбор нескольких вариантов ответа (с кнопками)'),
+    (COMPOSITE, 'Развернутый ответ текстом или картинкой'),
+]
+
+
+# Модель задания
+class Question(models.Model):
+    # основное
+    question_text = MarkdownxField(verbose_name='Текст вопроса')
+    explanation_text = MarkdownxField(verbose_name='Комментарий (пояснение) к вопросу', blank=True)
+
+    # картинки
+    image = models.ImageField(
+        verbose_name='Картинка',
+        upload_to=get_question_image_directory_path,
+        null=True,
+        blank=True
+    )
+    explanation_image = models.ImageField(
+        verbose_name='Картинка в пояснении',
+        upload_to=get_question_image_directory_path,
+        null=True,
+        blank=True
+    )
+
+    # оценивание
+    max_score = models.SmallIntegerField(
+        verbose_name='Максимальный балл',
+        default=1,
+    )
+
+    checking_policy = models.SmallIntegerField(
+        verbose_name='Политика проверки заданий',
+        choices=POLICY_CHOICES,
+        default=ONE_WRONG_MINUS_ONE,
+    )
+
+    # For graph
+    nodes = models.ManyToManyField('graph.Node', blank=True, null=True)
+
+    # For exam tree
+    themes = models.ManyToManyField('tags.ThemeTag', blank=True)
+
+    class Meta:
+        verbose_name = 'задание'
+        verbose_name_plural = 'задания'
+
+    def __str__(self):
+        return f'#{self.pk}. {self.question_text[:30]}...'
+
+    def check_answer(self, answer):
+        return 0
+
+
+def option_image_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/...
+    return 'questions/question_{0}/options/{1}/{2}'.format(
+        instance.question.pk,
+        instance.pk,
+        filename)
+
+
+# вариант ответа
+class QuestionOption(Question):
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE
+    )
+
+    is_true = models.BooleanField(
+        verbose_name='Правильный?',
+        default=False
+    )
+
+    option_text = MarkdownxField(
+        verbose_name='Текст варианта с разметкой',
+        null=True,
+        blank=True
+    )
+
+    # дополнительно
+    option_image = models.ImageField(
+        verbose_name='Картинка',
+        upload_to=option_image_directory_path,
+        null=True,
+        blank=True
+    )
+    help_text = MarkdownxField(
+        verbose_name='Подсказка',
+        max_length=300,
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = 'вариант ответа на задание'
+        verbose_name_plural = 'варианты ответа на задания'
+
+    def __str__(self):
+        return self.option_text
+
--- server/conf/settings.py
+++ server/conf/settings.py
@@ -37,8 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Bot
     'bot',
-    'users',
+
+    # Modules
+    'rest_framework',
+    'markdownx',
+
+    # My apps
+    'apps.questions',
+    'apps.users',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +136,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Rest Framework
+# https://www.django-rest-framework.org/
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
