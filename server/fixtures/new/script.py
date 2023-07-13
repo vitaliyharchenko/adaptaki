e135d5bc515f9bc664d08937f0e0b7db764dbaa4
Vitaliy Harchenko <harchenko.grape@gmail.com>
2023-07-13 14:45:59 +0500

    add exam questions

--- server/fixtures/new/script.py
+++ server/fixtures/new/script.py
@@ -7,57 +7,155 @@ q = open('questions.json')
 
 data_q = json.load(q)
 
-new_questions = []
+new_questions = [] 
 new_options = []
 
 # float questions
-q_float = open('float_questions.json')
-data_q_float = json.load(q_float)
+# q_float = open('float_questions.json')
+# data_q_float = json.load(q_float)
 
-for q_f in data_q_float:
-    new_question = {
-        'model': 'questions.question',
-        'fields': {
+# for q_f in data_q_float:
+#     new_question = {
+#         'model': 'questions.question',
+#         'fields': {
 
-        }
-    }
+#         }
+#     }
 
-    # забираем данные из родительского вопроса
-    for q in data_q:
-        if q['pk'] == q_f['pk']:
-            q_fields = q['fields']
-            new_question['pk'] = q['pk']
-            new_question['fields']['question_text'] = q['fields']['question_text']
-            new_question['fields']['explanation_text'] = q['fields']['explanation_text']
-            new_question['fields']['nodes'] = q['fields']['nodes']
-            new_question['fields']['trainer_tags'] = q['fields']['themes']
+#     # забираем данные из родительского вопроса
+#     for q in data_q:
+#         if q['pk'] == q_f['pk']:
+#             q_fields = q['fields']
+#             new_question['pk'] = q['pk']
+#             new_question['fields']['question_text'] = q['fields']['question_text']
+#             new_question['fields']['explanation_text'] = q['fields']['explanation_text']
+#             new_question['fields']['nodes'] = q['fields']['nodes']
+#             new_question['fields']['trainer_tags'] = q['fields']['themes']
     
-    fields = q_f['fields']
+#     fields = q_f['fields']
 
-    new_question['fields']['max_score'] = 1
-    new_question['fields']['checking_policy'] = 1
-    new_question['fields']['type'] = 1
+#     new_question['fields']['max_score'] = 1
+#     new_question['fields']['checking_policy'] = 1
+#     new_question['fields']['type'] = 1
 
-    new_questions.append(new_question)
+#     new_questions.append(new_question)
 
-    new_option = {
-        'model': 'questions.questionoption',
-        'pk': len(new_options)+1,
-        'fields': {
-            'question': q_f['pk'],
-            'is_true': True,
-            'option_text': fields['right_answer']
-        }
-    }
+#     new_option = {
+#         'model': 'questions.questionoption',
+#         'pk': len(new_options)+1,
+#         'fields': {
+#             'question': q_f['pk'],
+#             'is_true': True,
+#             'option_text': fields['right_answer']
+#         }
+#     }
 
-    new_options.append(new_option)
+#     new_options.append(new_option)
 
 
 # string questions
-q_string = open('string_questions.json')
-data_q_string = json.load(q_string)
+# q_string = open('string_questions.json')
+# data_q_string = json.load(q_string)
 
-for q_f in data_q_string:
+# for q_f in data_q_string:
+#     new_question = {
+#         'model': 'questions.question',
+#         'fields': {
+
+#         }
+#     }
+
+#     # забираем данные из родительского вопроса
+#     for q in data_q:
+#         if q['pk'] == q_f['pk']:
+#             q_fields = q['fields']
+#             new_question['pk'] = q['pk']
+#             new_question['fields']['question_text'] = q['fields']['question_text']
+#             new_question['fields']['explanation_text'] = q['fields']['explanation_text']
+#             new_question['fields']['nodes'] = q['fields']['nodes']
+#             new_question['fields']['trainer_tags'] = q['fields']['themes']
+    
+#     fields = q_f['fields']
+
+#     new_question['fields']['max_score'] = 1
+#     new_question['fields']['checking_policy'] = 1
+#     new_question['fields']['type'] = 1
+
+#     new_questions.append(new_question)
+
+#     new_option = {
+#         'model': 'questions.questionoption',
+#         'pk': len(new_options)+1,
+#         'fields': {
+#             'question': q_f['pk'],
+#             'is_true': True,
+#             'option_text': fields['right_answer']
+#         }
+#     }
+
+#     new_options.append(new_option)
+
+
+# print(new_questions[0])
+# print(new_options[1])
+
+
+# # exam questions
+# q_exam = open('exam_questions.json')
+# data_q_exam = json.load(q_exam)
+
+# pk = 6100
+
+# for q_f in data_q_exam:
+#     new_question = {
+#         'model': 'questions.question',
+#         'fields': {
+
+#         }
+#     }
+
+#     # забираем данные из родительского вопроса
+#     for q in data_q:
+#         if q['pk'] == q_f['pk']:
+#             q_fields = q['fields']
+#             new_question['pk'] = q['pk']
+#             new_question['fields']['question_text'] = q['fields']['question_text']
+#             new_question['fields']['explanation_text'] = q['fields']['explanation_text']
+#             new_question['fields']['nodes'] = q['fields']['nodes']
+#             new_question['fields']['trainer_tags'] = q['fields']['themes']
+    
+#     fields = q_f['fields']
+
+#     new_question['fields']['max_score'] = fields['maximum']
+#     if fields['order_importance']:
+#         new_question['fields']['type'] = 2
+#     else:
+#         new_question['fields']['type'] = 3
+#     new_question['fields']['checking_policy'] = 1
+
+#     new_questions.append(new_question)
+
+#     new_option = {
+#         'model': 'questions.questionoption',
+#         'pk': pk+1,
+#         'fields': {
+#             'question': q_f['pk'],
+#             'is_true': True,
+#             'option_text': fields['right_answer']
+#         }
+#     }
+
+#     pk += 1
+
+#     new_options.append(new_option)
+
+# exam questions
+q_exam = open('choice_questions.json')
+data_q_exam = json.load(q_exam)
+
+pk = 6100
+
+for q_f in data_q_exam:
     new_question = {
         'model': 'questions.question',
         'fields': {
@@ -77,28 +175,32 @@ for q_f in data_q_string:
     
     fields = q_f['fields']
 
-    new_question['fields']['max_score'] = 1
+    new_question['fields']['max_score'] = fields['maximum']
+    if fields['order_importance']:
+        new_question['fields']['type'] = 2
+    else:
+        new_question['fields']['type'] = 3
     new_question['fields']['checking_policy'] = 1
-    new_question['fields']['type'] = 1
 
     new_questions.append(new_question)
 
     new_option = {
         'model': 'questions.questionoption',
-        'pk': len(new_options)+1,
+        'pk': pk+1,
         'fields': {
             'question': q_f['pk'],
             'is_true': True,
             'option_text': fields['right_answer']
         }
     }
+
+    pk += 1
 
     new_options.append(new_option)
 
 
 print(new_questions[0])
-print(new_options[1])
-
+print(new_options[0])
 
 
 json_string = json.dumps(new_questions)
