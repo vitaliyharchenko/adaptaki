bf49a9b619e7709954e50deee3d9fcde907b3583
Vitaliy Harchenko <harchenko.grape@gmail.com>
2023-07-26 16:42:50 +0500

    token auth ready

--- server/apps/users/urls.py
+++ server/apps/users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 
 urlpatterns = [
     path('check/telegram/<int:telegram_id>/',
          views.GetUserByTelegramId.as_view()),
+    path('telegram-token-auth/',
+         views.GetTokenByTelegramId.as_view()),
 ]
--- server/apps/users/views.py
+++ server/apps/users/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework import permissions
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
 from django.http import Http404
 
 from .models import User
@@ -27,3 +29,39 @@ class GetUserByTelegramId(APIView):
         user = self.get_object(telegram_id)
         serializer = UserSerializer(user)
         return Response(serializer.data)
+
+
+class GetTokenByTelegramId(APIView):
+    """
+    Получение токена пользователя по его идентефикатору в Telegram
+
+    Доступно только для бота
+    """
+    permission_classes = [permissions.AllowAny]
+
+    def get_object(self, telegram_id):
+        try:
+            user = User.objects.get(telegram_id=telegram_id)
+            return Token.objects.get(user=user)
+        except User.DoesNotExist:
+            raise Http404
+        except Token.DoesNotExist:
+            token = Token.objects.create(user=user)
+            return token.key
+
+    def post(self, request, format=None):
+        telegram_id = request.data["telegram_id"]
+
+        key = ''
+
+        try:
+            user = User.objects.get(telegram_id=telegram_id)
+            token = Token.objects.get(user=user)
+            key = token.key
+        except User.DoesNotExist:
+            return Response({'error': 'user does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+        except Token.DoesNotExist:
+            token = Token.objects.create(user=user)
+            key = token.key
+
+        return Response({'token': key}, status=status.HTTP_200_OK)
